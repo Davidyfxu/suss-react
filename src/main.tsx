17b6b5d8fb06dd276e81b299f15b1f8e4a3c53d2
davidyfxu <82054510@qq.com>
2024-09-11 21:24:51 +0800

    feat: posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import ReactDOM from "react-dom/client";
-import "./main.css";
-import App from "./index.tsx";
+import ReactDOM from 'react-dom/client';
+import './main.css';
+import App from './index.tsx';
+import posthog from 'posthog-js';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+posthog.init('phc_B2JMBBtoPuZwZHCminqFmrdWhFyb5eSCZ0iSJCMzDoV', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+});
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
