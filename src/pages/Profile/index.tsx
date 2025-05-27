648eb83e816c1fcbec3d701ced7373f32abedbb4
davidyfxu <s200048@e.ntu.edu.sg>
2025-05-27 09:25:55 +0800

    fix: update profile product logic

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -4,24 +4,60 @@ import { useState } from 'react';
 import { updateUser } from './api.ts';
 import { UserOutlined, MailOutlined, LockOutlined } from '@ant-design/icons';
 
+interface FormValues {
+  username: string;
+  email: string;
+  last_name: string;
+  password?: string;
+  confirmPwd?: string;
+}
+
 const Profile = () => {
-  const [form] = Form.useForm();
+  const [form] = Form.useForm<FormValues>();
   const username = useUserStore((state) => state.username);
   const email = useUserStore((state) => state.email);
   const fullName = useUserStore((state) => state.fullName);
   const [loading, setLoading] = useState(false);
+  const [formChanged, setFormChanged] = useState(false);
+
+  const handleValuesChange = () => {
+    const currentValues = form.getFieldsValue();
+    const initialValues: FormValues = {
+      username,
+      email,
+      last_name: fullName,
+      password: undefined,
+      confirmPwd: undefined
+    };
+
+    const hasChanges = Object.keys(currentValues).some(
+      (key) =>
+        currentValues[key as keyof FormValues] !==
+        initialValues[key as keyof FormValues]
+    );
+
+    setFormChanged(hasChanges);
+  };
 
   const handleSubmit = async () => {
     try {
       await form.validateFields();
       const { username, email, password, confirmPwd, last_name } =
         form.getFieldsValue();
-      setLoading(true);
-      if (password !== confirmPwd)
+
+      if (password && password !== confirmPwd) {
         return message.error(
           'Confirmation password must match the original password.'
         );
-      await updateUser({ username, email, password, last_name });
+      }
+
+      setLoading(true);
+      await updateUser({
+        username,
+        email,
+        password: password || undefined,
+        last_name
+      });
       setLoading(false);
       message.success('Update user successfully');
       setTimeout(() => window.location.reload(), 1000);
@@ -41,7 +77,13 @@ const Profile = () => {
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
           Update User
         </h2>
-        <Form name="basic" form={form} layout="vertical" className="space-y-4">
+        <Form
+          name="basic"
+          form={form}
+          layout="vertical"
+          className="space-y-4"
+          onValuesChange={handleValuesChange}
+        >
           <Spin spinning={loading}>
             <Form.Item label="User ID" name="username" initialValue={username}>
               <Input
@@ -81,7 +123,6 @@ const Profile = () => {
               label="Password"
               name="password"
               rules={[
-                { required: true, message: 'Please fill your password' },
                 {
                   pattern:
                     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/,
@@ -93,15 +134,35 @@ const Profile = () => {
               <Input.Password
                 prefix={<LockOutlined className="text-gray-400" />}
                 allowClear
-                placeholder="Please fill your password"
+                placeholder="Please fill your password (optional)"
                 className="rounded-md"
               />
             </Form.Item>
-            <Form.Item label="Confirm Password" name="confirmPwd">
+            <Form.Item
+              label="Confirm Password"
+              name="confirmPwd"
+              dependencies={['password']}
+              rules={[
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (
+                      !getFieldValue('password') ||
+                      !value ||
+                      getFieldValue('password') === value
+                    ) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error('The two passwords do not match!')
+                    );
+                  }
+                })
+              ]}
+            >
               <Input.Password
                 prefix={<LockOutlined className="text-gray-400" />}
                 allowClear
-                placeholder="Confirm your password"
+                placeholder="Confirm your password (optional)"
                 className="rounded-md"
               />
             </Form.Item>
@@ -109,6 +170,7 @@ const Profile = () => {
               block
               type="primary"
               onClick={handleSubmit}
+              disabled={!formChanged}
               className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white font-semibold py-2 rounded-md transition duration-300"
             >
               Update
