84d0ab34d42277c782a3f3822b820ef6352c1ade
davidyfxu <s200048@e.ntu.edu.sg>
2025-06-29 22:01:58 +0800

    fix: round centrality value to three decimal places in tooltip

--- src/pages/Dashboard/components/SocialInteraction/index.tsx
+++ src/pages/Dashboard/components/SocialInteraction/index.tsx
@@ -84,7 +84,7 @@ const SocialGraph: React.FC = () => {
           value: node.centrality,
           group: node.user_type,
           size: Math.sqrt(node.centrality) * 10,
-          title: `${node.id} Centrality: ${round(node.centrality, 2)}`
+          title: `${node.id} Centrality: ${round(node.centrality, 3)}`
         }))
       );
 
